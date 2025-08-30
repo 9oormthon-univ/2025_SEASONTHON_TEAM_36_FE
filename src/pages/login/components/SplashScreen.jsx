c5a7ad5e1aabf5339cdad155c84f6ee6a37fc195
jhdgo1225 <parkjh99365@gmail.com>
2025-08-30 19:59:18 +0900

    FEAT: 로그인 화면 한정 manifest.json 부분이 아닌 스플래시 화면 구현 완료

---
+++ src/pages/login/components/SplashScreen.jsx
@@ -0,0 +1,37 @@
+import styled, { keyframes } from 'styled-components';
+import AppTitle from './AppTitle';
+import CheerSentence from '../components/CheerSentence';
+import AppLogoWell from './AppLogoWell';
+
+const fadeOut = keyframes`
+	0% {
+		opacity: 1;
+	}
+	50% {
+		opacity: 1;
+	}
+	100% {
+		opacity: 0;
+	}
+`;
+
+const SplashScreenStyle = styled.div`
+  display: ${props => (props.$show ? 'flex' : 'none')};
+  flex-direction: column;
+  align-items: center;
+  animation: ${fadeOut} 1.5s forwards;
+  position: absolute;
+  background-color: white;
+`;
+
+const SplashScreen = ({ show }) => {
+  return (
+    <SplashScreenStyle $show={show}>
+      <AppTitle width="317" />
+      <CheerSentence>넌 이미 잘하고 있어.</CheerSentence>
+      <AppLogoWell />
+    </SplashScreenStyle>
+  );
+};
+
+export default SplashScreen;
