2735526970c0bbbab1e39b2db8646a3471ca2ba1
jhdgo1225 <parkjh99365@gmail.com>
2025-08-30 19:59:18 +0900

    FEAT: 로그인 화면 한정 manifest.json 부분이 아닌 스플래시 화면 구현 완료

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from 'react';
+import SplashScreen from './components/SplashScreen';
+import LoginScreen from './components/LoginScreen';
+import styled from 'styled-components';
+
+const LoginStyle = styled.div`
+  position: relative;
+`;
+
 const Login = () => {
-  return <div>Login</div>;
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    // 1초 동안 스플래시 유지 후 로그인 화면 표시
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <LoginStyle>
+      <SplashScreen show={showSplash}>스플래시 화면</SplashScreen>
+      <LoginScreen>로그인 화면</LoginScreen>
+    </LoginStyle>
+  );
 };
 
 export default Login;
