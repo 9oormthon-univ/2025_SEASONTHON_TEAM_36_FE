2c8e94bd9c0b66d5ff0bea340f5b50a844ae09ea
jhdgo1225 <parkjh99365@gmail.com>
2025-08-31 10:50:15 +0900

    CHORE: NGROK 설정 제거

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    server: {
-      allowedHosts: [env.VITE_NGROK_DOMAIN],
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
