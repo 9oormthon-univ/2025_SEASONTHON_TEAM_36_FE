7afee51fa5a47af44d568c86b2912d746dcbf454
twjin03 <twjindev@gmail.com>
2025-09-02 03:25:58 +0900

    TEST: task.mock 데이터 추가

---
+++ src/pages/home/store/tasks.mock.js
@@ -0,0 +1,35 @@
+// 더미 태스크 데이터 (DotIndicator 5칸에 맞춰 5개)
+const tasks = [
+  {
+    dday: "D - 5",
+    title: "LG 전자제품 IMC 기획서 작성",
+    cheer: "느려도 괜찮아, 계속 가면 결국 도착해.",
+    progress: 15,
+  },
+  {
+    dday: "D - 4",
+    title: "시장조사 정리",
+    cheer: "오늘도 한 걸음!",
+    progress: 32,
+  },
+  {
+    dday: "D - 3",
+    title: "아이디어 보드 리팩터",
+    cheer: "꾸준함이 이긴다.",
+    progress: 48,
+  },
+  {
+    dday: "D - 2",
+    title: "슬라이드 시안 1차",
+    cheer: "시작이 반!",
+    progress: 71,
+  },
+  {
+    dday: "D - 1",
+    title: "최종 점검",
+    cheer: "좋아, 거의 다 왔어.",
+    progress: 90,
+  },
+];
+
+export default tasks;
