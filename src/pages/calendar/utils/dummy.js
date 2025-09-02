8d73ad76f5d47f72bd318eddd6f09a312fa04920
jhdgo1225 <parkjh99365@gmail.com>
2025-09-02 19:59:19 +0900

    TEST: 하드코딩용 To-Do 데이터

---
+++ src/pages/calendar/utils/dummy.js
@@ -0,0 +1,367 @@
+export const dummy8 = {
+  1: {
+    goal1: {
+      step1: { id: 201, done: true },
+      step2: { id: 202, done: false },
+    },
+  },
+  2: {
+    goal1: {
+      step1: { id: 203, done: false },
+    },
+    goal2: {
+      step1: { id: 204, done: true },
+      step2: { id: 205, done: false },
+    },
+  },
+  3: {
+    goal1: {
+      step1: { id: 206, done: true },
+    },
+  },
+  4: {
+    goal1: {
+      step1: { id: 207, done: false },
+      step2: { id: 208, done: true },
+    },
+  },
+  5: {
+    goal1: {
+      step1: { id: 209, done: false },
+    },
+    goal2: {
+      step1: { id: 210, done: true },
+    },
+  },
+  6: {
+    goal1: {
+      step1: { id: 211, done: false },
+      step2: { id: 212, done: true },
+      step3: { id: 213, done: false },
+    },
+  },
+  7: {
+    goal1: {
+      step1: { id: 214, done: true },
+    },
+  },
+  8: {
+    goal1: {
+      step1: { id: 215, done: false },
+      step2: { id: 216, done: true },
+    },
+  },
+  9: {
+    goal1: {
+      step1: { id: 217, done: false },
+    },
+  },
+  10: {
+    goal1: {
+      step1: { id: 218, done: true },
+      step2: { id: 219, done: false },
+    },
+  },
+  11: {
+    goal1: {
+      step1: { id: 220, done: false },
+    },
+  },
+  12: {
+    goal1: {
+      step1: { id: 221, done: true },
+      step2: { id: 222, done: false },
+    },
+  },
+  13: {
+    goal1: {
+      step1: { id: 223, done: true },
+    },
+  },
+  14: {
+    goal1: {
+      step1: { id: 224, done: false },
+      step2: { id: 225, done: true },
+    },
+  },
+  15: {
+    goal1: {
+      step1: { id: 226, done: false },
+    },
+    goal2: {
+      step1: { id: 227, done: true },
+    },
+  },
+  16: {
+    goal1: {
+      step1: { id: 228, done: false },
+      step2: { id: 229, done: true },
+    },
+  },
+  17: {
+    goal1: {
+      step1: { id: 230, done: false },
+    },
+  },
+  18: {
+    goal1: {
+      step1: { id: 231, done: true },
+      step2: { id: 232, done: false },
+    },
+  },
+  19: {
+    goal1: {
+      step1: { id: 233, done: false },
+    },
+  },
+  20: {
+    goal1: {
+      step1: { id: 234, done: true },
+      step2: { id: 235, done: false },
+    },
+  },
+  21: {
+    goal1: {
+      step1: { id: 236, done: false },
+    },
+  },
+  22: {
+    goal1: {
+      step1: { id: 237, done: true },
+      step2: { id: 238, done: false },
+    },
+  },
+  23: {
+    goal1: {
+      step1: { id: 239, done: true },
+    },
+  },
+  24: {
+    goal1: {
+      step1: { id: 240, done: false },
+      step2: { id: 241, done: true },
+    },
+  },
+  25: {
+    goal1: {
+      step1: { id: 242, done: false },
+    },
+  },
+  26: {
+    goal1: {
+      step1: { id: 243, done: true },
+      step2: { id: 244, done: false },
+    },
+  },
+  27: {
+    goal1: {
+      step1: { id: 245, done: false },
+    },
+  },
+  28: {
+    goal1: {
+      step1: { id: 246, done: true },
+      step2: { id: 247, done: false },
+    },
+  },
+  29: {
+    goal1: {
+      step1: { id: 248, done: true },
+    },
+  },
+  30: {
+    goal1: {
+      step1: { id: 249, done: false },
+      step2: { id: 250, done: true },
+    },
+  },
+  31: {
+    goal1: {
+      step1: { id: 251, done: false },
+    },
+  },
+};
+
+export const dummy9 = {
+  2: {
+    goal1: {
+      step1: { id: 104, done: true },
+    },
+    goal2: {
+      step1: { id: 301, done: true },
+      step2: { id: 302, done: true },
+      step3: { id: 303, done: true },
+    },
+    goal3: {
+      step1: { id: 401, done: false },
+      step2: { id: 402, done: true },
+    },
+  },
+  3: {
+    goal1: {
+      step1: { id: 105, done: false },
+      step2: { id: 106, done: false },
+      step3: { id: 107, done: true },
+    },
+  },
+  4: {
+    goal1: {
+      step1: { id: 108, done: true },
+    },
+    goal2: {
+      step1: { id: 109, done: true },
+      step2: { id: 110, done: true },
+    },
+  },
+  5: {
+    goal1: {
+      step1: { id: 111, done: false },
+    },
+  },
+  6: {
+    goal1: {
+      step1: { id: 112, done: true },
+      step2: { id: 113, done: false },
+    },
+  },
+  7: {
+    goal1: {
+      step1: { id: 114, done: true },
+    },
+  },
+  8: {
+    goal1: {
+      step1: { id: 115, done: false },
+      step2: { id: 116, done: false },
+    },
+  },
+  9: {
+    goal1: {
+      step1: { id: 117, done: true },
+    },
+  },
+  10: {
+    goal1: {
+      step1: { id: 118, done: false },
+      step2: { id: 119, done: true },
+      step3: { id: 120, done: false },
+    },
+  },
+  11: {
+    goal1: {
+      step1: { id: 121, done: true },
+    },
+  },
+  12: {
+    goal1: {
+      step1: { id: 122, done: false },
+      step2: { id: 123, done: false },
+    },
+  },
+  13: {
+    goal1: {
+      step1: { id: 124, done: true },
+    },
+    goal2: {
+      step1: { id: 125, done: false },
+    },
+  },
+  14: {
+    goal1: {
+      step1: { id: 126, done: false },
+      step2: { id: 127, done: true },
+    },
+  },
+  15: {
+    goal1: {
+      step1: { id: 128, done: false },
+    },
+  },
+  16: {
+    goal1: {
+      step1: { id: 129, done: true },
+      step2: { id: 130, done: false },
+    },
+  },
+  17: {
+    goal1: {
+      step1: { id: 131, done: false },
+    },
+  },
+  18: {
+    goal1: {
+      step1: { id: 132, done: true },
+      step2: { id: 133, done: true },
+    },
+  },
+  19: {
+    goal1: {
+      step1: { id: 134, done: false },
+    },
+  },
+  20: {
+    goal1: {
+      step1: { id: 135, done: true },
+      step2: { id: 136, done: false },
+    },
+  },
+  21: {
+    goal1: {
+      step1: { id: 137, done: false },
+    },
+  },
+  22: {
+    goal1: {
+      step1: { id: 138, done: true },
+      step2: { id: 139, done: true },
+    },
+    goal2: {
+      step1: { id: 501, done: true },
+      step2: { id: 502, done: true },
+    },
+  },
+  23: {
+    goal1: {
+      step1: { id: 140, done: false },
+    },
+  },
+  24: {
+    goal1: {
+      step1: { id: 141, done: true },
+      step2: { id: 142, done: false },
+    },
+  },
+  25: {
+    goal1: {
+      step1: { id: 143, done: false },
+    },
+  },
+  26: {
+    goal1: {
+      step1: { id: 144, done: true },
+      step2: { id: 145, done: false },
+    },
+  },
+  27: {
+    goal1: {
+      step1: { id: 146, done: false },
+    },
+  },
+  28: {
+    goal1: {
+      step1: { id: 147, done: true },
+      step2: { id: 148, done: true },
+    },
+  },
+  29: {
+    goal1: {
+      step1: { id: 149, done: false },
+    },
+  },
+  30: {
+    goal1: {
+      step1: { id: 150, done: true },
+      step2: { id: 151, done: false },
+    },
+  },
+};
