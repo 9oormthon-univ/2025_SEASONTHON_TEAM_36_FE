9ae4b64c5f83e1a820f66a95bae65721aebc8ee2
jhdgo1225 <parkjh99365@gmail.com>
2025-09-08 19:46:24 +0900

    FIX: 디버그 console log 재확인

--- src/pages/calendar/components/CustomCalendar.jsx
+++ src/pages/calendar/components/CustomCalendar.jsx
@@ -66,7 +66,7 @@ const CustomCalendar = ({
   const getTileContent = ({ activeStartDate, date, view }) => {
     // 월 보기일 때만 div 추가
     if (view === "month") {
-      const tmpPercentage = percentageOfDay[dateToFormatString(date)];
+      const tmpPercentage = percentage && percentageOfDay[dateToFormatString(date)];
       console.log(`${dateToFormatString(date)}의 step 진행율: ${percentageOfDay && tmpPercentage}`);
       const isSameDate = percentageOfDay
         ? (tmpPercentage !== null && tmpPercentage !== undefined)
