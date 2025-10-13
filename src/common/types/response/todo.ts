3170b3a0653653f5e2d195b6864566c674fd8760
jhdgo1225 <parkjh99365@gmail.com>
2025-10-13 12:30:59 +0900

    REFACTOR: API Request, Response 타입 정의

---
+++ src/common/types/response/todo.ts
@@ -0,0 +1,39 @@
+/**
+ * End-Point: [PUT] /api/v1/todos/{todoId}
+ * Step 수정
+ * src/apis/todo.ts의 updateTodo
+ *
+ * End-Point: [PUT] /api/v1/todos/{todoId}/complete
+ * Todo 완료
+ * src/apis/todo.ts의 updateTodoComplete
+ *
+ * End-Point: [POST] /api/v1/todos
+ * Todo 추가
+ * src/apis/todo.ts의 addTodo
+ */
+
+import { RespStepInfo } from "./step";
+
+export interface RespTodo {
+  currentDate: string;
+  id: number;
+  userId: number;
+  dDay: string;
+  title: string;
+  warmMessage: string;
+  progress: number;
+  isCompleted: boolean;
+  stepResponses: Array<RespStepInfo>;
+}
+
+/**
+ * End-Point: [GET] /api/v1/todos
+ * 회원의 Todo 조회
+ * src/apis/todo.ts의 fetchTodos
+ */
+
+export interface RespAllTodo {
+  pages: number;
+  total: number;
+  contents: Array<RespTodo>;
+}
