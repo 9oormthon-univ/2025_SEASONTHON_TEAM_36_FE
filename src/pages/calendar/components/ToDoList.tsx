9a00ede3ad14473146ae970231784f556cbd7b93
jhdgo1225 <parkjh99365@gmail.com>
2025-10-16 23:02:18 +0900

    REFACTOR: 스타일 코드 src/pages/calendar/styles/Goal.tsx로 이전

--- src/pages/calendar/components/ToDoList.tsx
+++ src/pages/calendar/components/ToDoList.tsx
@@ -1,13 +1,7 @@
-import styled from "styled-components";
-
 import { Title } from "../styles";
+import { ToDoListStyle } from "../styles/Goal";
 import { ListProps } from "../types/props";
 import GoalList from "./GoalList";
-
-const ToDoListStyle = styled.div`
-  margin-top: 10px;
-  padding: 0 26px 90px 26px;
-`;
 
 const ToDoList = ({ toDo, handleModifyStep, handleDeleteStep }: ListProps) => {
   return (
