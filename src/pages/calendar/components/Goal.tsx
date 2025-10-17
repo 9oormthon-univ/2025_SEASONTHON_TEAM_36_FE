32dc6b49540780b4c29f807168925eebf01a9b8a
jhdgo1225 <parkjh99365@gmail.com>
2025-10-17 12:45:15 +0900

    REFACTOR: Step 컴포넌트 파일로 분리

--- src/pages/calendar/components/Goal.tsx
+++ src/pages/calendar/components/Goal.tsx
@@ -1,26 +1,15 @@
-import { StepContent } from "@mui/material";
-import { useEffect, useState } from "react";
-
 import DotImg from "../../../assets/images/dot.png";
-import { Input } from "../styles";
 import { GoalContainer, GoalName, GoalStyle } from "../styles/Goal";
-import { Row, StepCheckBox, StepList } from "../styles/Step";
-import { GoalProps } from "../types/props";
-import type { Step } from "../types/ToDo";
-import StepManager from "./StepManager";
+import { StepList } from "../styles/Step";
+import type { GoalProps } from "../types/props";
+import type { StepType } from "../types/ToDo";
+import Step from "./Step";
 
 const GoalDivider = () => {
   return <img src={DotImg} alt="Goal 강조점" width="6" height="6" style={{ marginRight: "6px" }} />;
 };
 
-const Goal = ({ goalId, goal, steps, handleModifyStep, handleDeleteStep }: GoalProps) => {
-  const [updateSteps, setUpdateSteps] = useState<Step[]>([]);
-  const [isModify, setIsModify] = useState<boolean[]>(steps.map(_ => false));
-
-  useEffect(() => {
-    setUpdateSteps(steps);
-  }, [steps]);
-
+const Goal = ({ goalId, goal, steps }: GoalProps) => {
   return (
     <GoalStyle>
       <GoalContainer>
@@ -28,40 +17,9 @@ const Goal = ({ goalId, goal, steps, handleModifyStep, handleDeleteStep }: GoalP
         <GoalName>{goal}</GoalName>
       </GoalContainer>
       <StepList>
-        {steps.map((step, index: number) => {
-          return (
-            <Row key={step.id}>
-              <StepContent>
-                <StepCheckBox $did={step.done} />
-                <Input
-                  type="text"
-                  value={updateSteps[index]?.name ?? ""}
-                  disabled={!isModify[index]}
-                  onChange={e => {
-                    const tmpSteps = [...updateSteps];
-                    tmpSteps[index].name = e.target.value;
-                    setUpdateSteps(tmpSteps);
-                  }}
-                  $fontSize={"var(--fs-md)"}
-                />
-              </StepContent>
-              <StepManager
-                isModify={isModify[index]}
-                setIsModify={() => {
-                  const tmp = [...isModify];
-                  tmp[index] = !tmp[index];
-                  setIsModify(tmp);
-                }}
-                handleModifyStep={() => {
-                  handleModifyStep(goalId, step.id, updateSteps[index].name);
-                }}
-                handleDeleteStep={() => {
-                  handleDeleteStep(goalId, step.id);
-                }}
-              />
-            </Row>
-          );
-        })}
+        {steps.map((step: StepType, index: number) => (
+          <Step key={index} goalId={goalId} step={step} />
+        ))}
       </StepList>
     </GoalStyle>
   );
