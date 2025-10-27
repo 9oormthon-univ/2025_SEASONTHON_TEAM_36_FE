c05229c465a7578a3fef98cd7ffef46ae3ec9596
jhdgo1225 <parkjh99365@gmail.com>
2025-10-28 00:45:42 +0900

    REFACTOR: import된 img 파일 string으로 타입 assert

--- src/pages/profile/components/Button.tsx
+++ src/pages/profile/components/Button.tsx
@@ -14,7 +14,7 @@ const Button = ({
       onClick={() => {
         handleMoveMonth(move === "left" ? -1 : 1);
       }}
-      src={move === "left" ? leftImg : rightImg}
+      src={move === "left" ? (leftImg as string) : (rightImg as string)}
       alt={move === "left" ? "왼쪽" : "오른쪽"}
       width="23"
       height="23"
