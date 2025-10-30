aafb9dfa221e7b0368947176f51cab40d9d4634f
jhdgo1225 <parkjh99365@gmail.com>
2025-10-30 20:30:50 +0900

    FEAT: 다른 요소 클릭 시 수정 상태, 옵션 활성화 상태 false로 초기화

---
+++ src/pages/calendar/hooks/useOnClickOutside.ts
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+type RefType = React.RefObject<HTMLElement | null>;
+
+export const useOnClickOutside = (
+  ref: RefType,
+  handler: (event: MouseEvent | TouchEvent) => void,
+) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+};
