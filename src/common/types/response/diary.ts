49c3a348609d13efbb6ceae5cbedbdf9e39e8d9c
twjin03 <twjindev@gmail.com>
2025-11-03 19:13:27 +0900

    REFACTOR: RespDiaryDetail 타입 일부 수정 (enum 추가)

--- src/common/types/response/diary.ts
+++ src/common/types/response/diary.ts
@@ -4,21 +4,39 @@ export interface RespDiary {
   mood: string;
 }
 
+// RespDiaryDetail 필드 안 enum 값들
+type Place = "HOME" | "WORK" | "CAFE" | "LIBRARY" | "CLASSROOM" | "OTHER";
+
+type Mood =
+  | "HAPPY"
+  | "EXCITED"
+  | "CALM"
+  | "NORMAL"
+  | "THRILLING"
+  | "FRUSTRATED"
+  | "DEPRESSED"
+  | "EMPTY"
+  | "ANGRY"
+  | "DISAPPOINTED";
+
+type CompletionLevel = "ZERO" | "TWENTY_FIVE" | "FIFTY" | "SEVENTY_FIVE" | "ONE_HUNDRED";
 /** [GET] /api/v1/diaries/detail: 특정 달의 Diary 상세 조회 (쿼리 파라미터: date -> "yyyy-MM-dd" 형식) */
 export interface RespDiaryDetail {
+  /** "yyyy-MM-dd" */
   date: string;
-  todayCompletedTodoResponses: TodayCompletedTodo[];
+  todayCompletedTodoResponses: TodayCompletedTodo[]; // 추후 수정 필요
   emotion: number;
   energy: number;
-  place: string;
-  mood: string;
+  place: Place; // enum
+  mood: Mood; // enum
   focusLevel: number;
-  completionLevel: string;
+  completionLevel: CompletionLevel; // enum
   memo: string;
   photoUrl: string;
 }
 
 interface TodayCompletedTodo {
+  // 추후 수정 필요
   todoId: number;
   todoTitle: string;
   processTime: ProcessTime;
