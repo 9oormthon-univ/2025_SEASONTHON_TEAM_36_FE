bd9050c4d2d1c52ade742588741dd39555f335df
jhdgo1225 <parkjh99365@gmail.com>
2025-11-09 00:35:32 +0900

    FIX: 회원가입 완료 페이지에서 홈으로 이동하는 동작 문제 해결 시도

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,17 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import WellImg from "@/assets/images/well.png";
 
 import { Button, Message, Page } from "./styles/SignUp";
 
 const SignUpDone = () => {
+  const navigate = useNavigate();
   return (
     <Page>
-      <img src={WellImg} alt="우물" width="208" />
+      <img src={WellImg as string} alt="우물" width="208" />
       <Message>{"회원가입이 완료되었습니다.\n우물 밖으로 나갈\n준비가 되셨나요?"}</Message>
-      <Button>
-        <Link to="/home">우물 밖으로 나가기</Link>
-      </Button>
+      <Button onClick={() => void navigate("/home")}>우물 밖으로 나가기</Button>
     </Page>
   );
 };
