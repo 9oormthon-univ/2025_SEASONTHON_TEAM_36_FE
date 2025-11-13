95cc613105e2bb588e7e7451af79508ed747ae10
jhdgo1225 <parkjh99365@gmail.com>
2025-11-13 22:12:39 +0900

    FEAT: 스와이프 동작으로 react calendar 왼쪽, 오른쪽 버튼 클릭

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -2,6 +2,9 @@ import "./styles/calendar.css";
 
 import { useEffect } from "react";
 
+import ContentWrapper from "@/common/components/PageSwiper";
+import { useSwipeGesture } from "@/common/hooks/useSwipeGesture";
+
 import CustomCalendar from "./components/CustomCalendar";
 import GoalList from "./components/GoalList";
 import { useInitAllTodo } from "./hooks/useInitAllTodo";
@@ -11,6 +14,24 @@ import { ToDo } from "./styles/Goal";
 
 const CalendarScreen = () => {
   const view = useCalendar(state => state.view);
+  const handleMoveMonth = useCalendar(state => state.handleMoveMonth);
+  const {
+    swipeHandlers,
+    dragOffset,
+    isDragging,
+    swipeStatus,
+    isTransitioning,
+    setIsTransitioning,
+    setSwipeStatus,
+  } = useSwipeGesture({
+    onSwipeLeft: () => {
+      setIsTransitioning(true);
+    },
+    onSwipeRight: () => {
+      setIsTransitioning(true);
+    },
+    minSwipeDistance: 50,
+  });
   useInitAllTodo();
 
   useEffect(() => {
@@ -25,16 +46,40 @@ const CalendarScreen = () => {
     }
   }, []);
 
+  useEffect(() => {
+    const calendarLeftLabelBtn = document.querySelector(
+      ".react-calendar__navigation__prev-button",
+    ) as HTMLElement;
+    const calendarRightLabelBtn = document.querySelector(
+      ".react-calendar__navigation__next-button",
+    ) as HTMLElement;
+    if (swipeStatus < 0) {
+      calendarLeftLabelBtn?.click();
+      handleMoveMonth(-1);
+    } else if (swipeStatus > 0) {
+      calendarRightLabelBtn?.click();
+      handleMoveMonth(1);
+    }
+    setSwipeStatus(0);
+  }, [swipeStatus, setSwipeStatus, handleMoveMonth]);
+
   return (
     <Page>
       <Main>
-        <CustomCalendar />
-        {view === "month" && (
-          <ToDo>
-            <Title $fontSize={"var(--fs-lg)"}>Task To-Do</Title>
-            <GoalList />
-          </ToDo>
-        )}
+        <ContentWrapper
+          $dragOffset={dragOffset}
+          $isDragging={isDragging}
+          $isTransitioning={isTransitioning}
+          {...swipeHandlers}
+        >
+          <CustomCalendar />
+          {view === "month" && (
+            <ToDo>
+              <Title $fontSize={"var(--fs-lg)"}>Task To-Do</Title>
+              <GoalList />
+            </ToDo>
+          )}
+        </ContentWrapper>
       </Main>
     </Page>
   );
